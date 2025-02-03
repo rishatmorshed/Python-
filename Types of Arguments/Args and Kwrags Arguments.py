0bc37a641b6d68536d3fe45bdd3db3f7810a53dd
Mahbub Morshed <45009800+rishatmorshed@users.noreply.github.com>
2025-02-03 21:16:30 +0600

    Update Args and Kwrags Arguments.py

--- Types of Arguments/Args and Kwrags Arguments.py
+++ Types of Arguments/Args and Kwrags Arguments.py
@@ -3,5 +3,5 @@ def info_person(*args, **kwrags):  #Key Word argument works with dictionary and 
         print(key, value)
     print(args)
 
-info_person(1,2, name = "Morshed", age = 30, dept = "cse")
+info_person(1,2, name = "Morshed", age = 22, dept = "cse")
 info_person(1,2,3, name = "Mahbub", dept="cse")
