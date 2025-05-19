a02422721ec34a2e3197224c6366481127de98e4
Mahbub Morshed <45009800+rishatmorshed@users.noreply.github.com>
2025-05-20 00:26:50 +0600

    Add files via upload

---
+++ 2D matrix userInput and print.py
@@ -0,0 +1,13 @@
+matrix = []
+col = 3
+rows = 3
+for i in range(col):
+    row = []
+    for j in range(rows):
+        val = int(input(f"Enter value for position[{i}][{j}]: "))
+        row.append(val)
+    matrix.append(row)
+for row in matrix:
+    for val in row:
+        print(val, end=' ')
+    print()
